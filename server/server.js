3161c6a5b49584818f754fcdf40941a1d2a17989
Big_J <107224194+Glocks99@users.noreply.github.com>
2024-12-19 10:49:42 +0000

    done

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express")
+require("./models/db")
+const cors = require("cors")
+const dotenv = require("dotenv")
+const transRoute = require("./routes/trans_route")
+const authRoute = require("./routes/authroute")
+const userRoute = require("./routes/user_route")
+dotenv.config()
+const cookieParser = require("cookie-parser")
+
+const app = express()
+
+const allowedOrigins = ["http://localhost:5173"]
+
+app.use(cors({origin: allowedOrigins, credentials: true}))
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use(cookieParser())
+app.use(transRoute)
+app.use("/api/auth", authRoute)
+app.use("/api/user", userRoute)
+
+app.listen(process.env.PORT, () => console.log("server is running"))
