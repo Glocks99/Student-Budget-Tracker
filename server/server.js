5f105f67e447237c5909ab6819af9ea9cde32d47
James <jameslarbie30@gmail.com>
2024-12-24 15:39:48 +0000

    fix cors

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-const allowedOrigins = ["https://student-budget-tracker.vercel.app"];
+const allowedOrigins = ["*"];
 
 app.use(cors({ origin: allowedOrigins, credentials: true, methods: "GET, POST, PUT, DELETE" }));
 app.use(express.urlencoded({ extended: false }));
